81e190eaf2c3b463347342ac5c9ce8b78ee6c0bb
Rogelio Menco <rogeliomenco4@gmail.com>
2024-03-02 14:56:52 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import AppContent from "./components/shared/AppContent";
+import AppFooter from "./components/shared/AppFooter";
+import AppHeader from "./components/shared/AppHeader";
+
+function App() {
+  return (
+    <>
+      <AppHeader />
+      <AppContent />
+      <AppFooter />
+    </>
+  );
+}
+
+export default App;
